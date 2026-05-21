4b90d5e397bb691084ca6f1b8b77b2f54ec18aa1
Caspersonn <lucakasper8@gmail.com>
2026-05-21 11:07:51 +0200

    Add csvview with default config

---
+++ config/plugs/csvview.nix
@@ -0,0 +1,16 @@
+{
+  plugins.csvview = {
+    enable = true;
+    autoload = true;
+    settings = {
+      parser = {
+        async_chunksize = 30;
+      };
+      view = {
+        display_mode = "border";
+        spacing = 4;
+        header_lnum = 1;
+      };
+    };
+  };
+}
