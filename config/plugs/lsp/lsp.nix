a745c7ff4b5e081a8b2c02cfcf12f925074b9051
Caspersonn <lucakasper8@gmail.com>
2026-09-08 07:27:52 +0200

    fix: remove deprecated function

--- config/plugs/lsp/lsp.nix
+++ config/plugs/lsp/lsp.nix
@@ -37,12 +37,12 @@
         },
       })
 
-      vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+      vim.lsp.handlers['textDocument/hover'] = vim.lsp.buf(
         vim.lsp.handlers.hover,
         {border = 'rounded'}
       )
 
-      vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+      vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.buf(
         vim.lsp.handlers.signature_help,
         {border = 'rounded'}
       )
